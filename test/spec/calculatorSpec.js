0d330e9a7db12224827d2a4c0be6ee56aaf84ec4
Karaignee <karen_rooney@hotmail.com>
2014-03-26 19:09:02 +0000

    add jasmine test files

---
+++ test/spec/calculatorSpec.js
@@ -0,0 +1,8 @@
+describe("My Calculator", function() {
+    it("should ", function() {
+        expect().toBe();
+    })
+    it("", function(){
+        expect(function() {("test")}).toThrow(new Error("Not a number"))
+    })
+})
